430fbb1a141dd67af1d408451306ca1b5e02b4c1
Tanveer khan <152361941+tanveerkhan00@users.noreply.github.com>
2024-08-02 09:37:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+// Assignment 1: Sum of Two Numbers
+
+// function addNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(addNumbers(2, 3)); // Output: 5
+// console.log(addNumbers(5, 1)); // Output: 6
+// console.log(addNumbers(10, 20)); // Output: 30
+
+
+
+
+
+// Assignment 2: Convert String to Uppercase
+
+// function toUpper(str) {
+//   return str.toUpperCase();
+// }
+// console.log(toUpper("hello")); // Output: "HELLO"
+// console.log(toUpper("world")); // Output: "WORLD"
+// console.log(toUpper("javascript")); // Output: "JAVASCRIPT"
+
+
+
+
+
+//  Assignment 3: Average of Numbers in an Array
+
+// function averageArray(arr) {
+//   const sum = arr.reduce((acc, num) => acc + num, 0);
+//   return sum / arr.length;
+// }
+// console.log(averageArray([1, 2, 3])); // Output: 2
+// console.log(averageArray([5, 5, 5])); // Output: 5
+// console.log(averageArray([10, 20, 30, 40])); // Output: 25
+
+
+
+
+// Assignment 4: Remove Vowels from a string
+
+// function removeVowels(str) {
+//   return str.replace(/[aeiouAEIOU]/g, '');
+// }
+// console.log(removeVowels("hello world")); // Output: "hll wrld"
+// console.log(removeVowels("javascript")); // Output: "jvscrpt"
+// console.log(removeVowels("AEIOUaeiou")); // Output: ""
+
+
+
+
+
+// Assignment 5: Factorial of a number
+
+// function factorial(n) {
+//   if (n === 0) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
+// }
+// console.log(factorial(5)); // Output: 120
+// console.log(factorial(3)); // Output: 6
+// console.log(factorial(0)); // Output: 1
+
+
+
+
+
+//Assignment 6  Reverse a sring
+// let reverseString ;
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+// console.log(reverseString("hello")); // Output: "olleh"
+// console.log(reverseString("world")); // Output: "dlrow"
+// console.log(reverseString("javascript")); // Output: "tpircsavaj"
+
+
